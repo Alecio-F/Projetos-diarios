5bc19c3a6c2444fb11e039941f5bdf13529c0cde
Aleksan20 <ciceroalecio30@gmail.com>
2022-12-21 21:01:22 -0300

    Outro menu

---
+++ menu-responsivo/assets/js/script03.js
@@ -0,0 +1,5 @@
+document.querySelector("barra-menu").addEventListener("click", () =>
+    document.querySelector('menu').classList.toggle('change'),
+    document.querySelector('nav').classList.toggle('change'),
+    document.querySelector('menu-bg').classList.toggle('change-bg')
+)
